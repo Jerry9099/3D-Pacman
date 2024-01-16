42b4207820d2523d497138008be9bcb7099c07b0
Jerry9099 <ebooks2222@gmail.com>
2024-01-15 23:28:44 -0800

    tranlucency

--- 3D-Pacman/player.gd
+++ 3D-Pacman/player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody
 
 export var max_speed = 5
+export var forward_velocity = Vector3(1, 0, 0)
 var velocity = Vector3()
 
 # Called when the node enters the scene tree for the first time.
@@ -11,11 +12,23 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = 1
+		rotation_degrees.y -= max_speed*10*delta
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -1
-	else:
-		velocity.x = 0
+		rotation_degrees.y += max_speed*10*delta
+
+#	if Input.is_action_pressed("ui_right")  && $Timer.is_stopped():
+#		rotation_degrees.y -= 10
+#		$Timer.start()
+#	elif Input.is_action_pressed("ui_left") && $Timer.is_stopped():
+#		rotation_degrees.y += 10
+#		$Timer.start()
+	
+#	if Input.is_action_pressed("ui_right")  && $Timer.is_stopped():
+#		velocity.x = 1
+#	elif Input.is_action_pressed("ui_left") && $Timer.is_stopped():
+#		velocity.x = -1
+#	else:
+#		velocity.x = 0
 		
 	if Input.is_action_pressed("ui_up"):
 		velocity.z = -1
@@ -29,4 +42,7 @@ func _process(delta):
 	else:
 		velocity.y = -1
 	
-	move_and_slide(max_speed*velocity)
+	move_and_slide(max_speed*velocity.rotated(Vector3(0, 1, 0), rotation.y))
+#	$Camera.global_transform = transform
+#	$Camera.global_translation += Vector3(0, 1.5, 2.3)
+#	$Camera.global_rotation.x += -15.5 
